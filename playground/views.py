6574e4bb56557a8f4f362a6d72c2f35d888828b2
55soup <w2118@e-mirim.hs.kr>
2023-03-23 14:48:01 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello, World");
+    return HttpResponse("Hello, World")
 
 def say_hello_html(request):
-    return render(request, "playground/hello.html");
+    return render(request, "playground/hello.html", {'name':'3반!!!', 'greeting' : '반갑다'})
 
 def say_bye(request):
-    return render(request, "playground/bye.html");
+    context = {
+        'name' : '박선주',
+        'bye' : '안녕히계세요',
+    }
+    return render(request, "playground/bye.html", context=context)
